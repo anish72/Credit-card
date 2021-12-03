f2cbab0106afd95578b35bdb5e649d89eb55dfd1
Anish kumar ranjan <anish727739@gmail.com>
2021-12-03 11:57:12 +0530

    commit done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
+import 'react-credit-cards/es/styles-compiled.css';
+import Cards from 'react-credit-cards';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [expiry, setExpiry] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [focus, setFocus] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Cards 
+          cvc={cvc}
+          expiry={expiry}
+          focused={focus}
+          name={name}
+          number={number}
+        />
+      <form>
+        <input type="tel" name="number" val={number} placeholder={"Card Number"} onChange={e => setNumber(e.target.value)}onFocus={e=>setFocus(e.target.name)}/>
+        <input type="tel" name="name" val={name} placeholder={"Cardholder Name"} onChange={e => setName(e.target.value)} onFocus={e=>setFocus(e.target.name)}/>
+        <input type="tel" name="expiry" val={expiry} placeholder={"Enter Expiry date"} onChange={e => setExpiry(e.target.value)} onFocus={e=>setFocus(e.target.name)}/>
+        <input type="tel" name="cvc" val={cvc} placeholder={"Enter Cvc"} onChange={e => setCvc(e.target.value)} onFocus={e=>setFocus(e.target.name)} />
+      </form>
     </div>
   );
 }
-
 export default App;
